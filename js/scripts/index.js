09e953aa20a2dc8c29073b2d95548b8a95ec1d48
steffkes <matheis.stefan@googlemail.com>
2012-04-04 10:52:57 +0200

    Additional check on system.uptime

--- js/scripts/index.js
+++ js/scripts/index.js
@@ -99,7 +99,7 @@ var system_info = function( element, system_data )
 {
   // -- usage
 
-  var load_average = system_data['system']['uptime'].match( /load average: (.+)/ );
+  var load_average = ( system_data['system']['uptime'] || '' ).match( /load average: (.+)/ );
   if( load_average && load_average[1] )
   {
     var hl = $( '#system h2', element );
